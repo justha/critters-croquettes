722b7deb7ed2fdc3b2c53a687bdb4fa8924c3d80
justha <nganha42@gmail.com>
2020-10-19 15:09:18 -0500

    added feed and string emthods

--- index.py
+++
@@ -1,5 +0,0 @@
-from flyers import Elephant, FlyingMonkey
-from hoppers import Kangaroo, Rabbit
-from slitherers import KingSnake
-from swimmers import Duck, Penguin
-from walkers import Alpaca, Donkey, Horse, Koala, Lemur, Llama, Pig, Sloth
